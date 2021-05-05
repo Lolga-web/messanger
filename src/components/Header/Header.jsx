1e4028d58edf94930efdae5884c3a9249ef3f3dd
Lolga-web <lolga.web@mail.ru>
2021-05-05 15:47:49 +1000

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { connect } from "react-redux";
+
+function Header(props) {
+    return <div className="header">
+        <h1>MyMessenger { isFinite(props.chatId) && ('- ' + props.chats[props.chatId].title) }</h1>
+    </div>;
+}
+
+const mapState = ({ chatsReducer }) => ({
+    chats: chatsReducer.chats
+});
+
+export default connect(mapState, null)(Header);
